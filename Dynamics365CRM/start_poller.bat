b4ffb9b034b19dfb8bdcef39e7211a9f9d7260c5
Nana Nyamekye <Nnyamekye@winmarkcorporation.com>
2026-04-09 14:13:04 -0500

    Updating Git

--- Dynamics365CRM/start_poller.bat
+++ Dynamics365CRM/start_poller.bat
@@ -1,3 +1,7 @@
 @echo off
 cd /d "%~dp0"
+REM Kill any existing poller processes
+for /f "tokens=2" %%a in ('wmic process where "commandline like '%%sharepoint_poller%%'" get processid /value 2^>nul ^| find "="') do (
+    taskkill /PID %%a /F >nul 2>&1
+)
 python sharepoint_poller.py >> poller_output.log 2>&1
